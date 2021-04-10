7183049e9c042cdbda4f8b726d1a5822df85ce24
Dzmitry Shyndzin <d.shyndzin@dzensoft.com>
2021-04-11 02:54:02 +0300

    Fix duplicated logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var createPattern = function (path) {
     return { pattern: path, included: true, served: true, watched: false };
 };
 
-var OrderReporter = function (config, baseReporterDecorator) {
+var OrderReporter = function (config, baseReporterDecorator, emitter) {
     const files = config.files;
 
     baseReporterDecorator(this);
@@ -13,9 +13,19 @@ var OrderReporter = function (config, baseReporterDecorator) {
         createPattern(__dirname + '/lib/jasmine-order.reporter.js'),
         createPattern(__dirname + '/lib/jasmine-order.adapter.js')
     );
+
+    const reporter = this; // self reference to use in callback
+
+    // see https://github.com/karma-runner/karma/issues/2192#issuecomment-290230042
+    emitter.on('browser_info', (browser, data) => {
+        if (!data || data.type !== 'Jasmine Order Reporter') {
+            return
+        }
+        reporter.onBrowserLog(browser, data.seedInfo, data.type);
+    });
 };
 
-OrderReporter.$inject = ['config', 'baseReporterDecorator'];
+OrderReporter.$inject = ['config', 'baseReporterDecorator', 'emitter'];
 
 module.exports = {
     "reporter:jasmine-order": ["type", OrderReporter]
